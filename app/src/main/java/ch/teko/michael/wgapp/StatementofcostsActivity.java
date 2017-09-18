f17ad7a408a5a31bc6df3c4f898e7d3431193657
David Weber <david.weber@w3tec.ch>
2017-09-18 13:30:23 +0200

    Some changes in settels

--- app/src/main/java/ch/teko/michael/wgapp/StatementofcostsActivity.java
+++ app/src/main/java/ch/teko/michael/wgapp/StatementofcostsActivity.java
@@ -1,8 +1,15 @@
 package ch.teko.michael.wgapp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.List;
+
+import ch.teko.michael.wgapp.api.RequestHelper;
+import ch.teko.michael.wgapp.model.Settle;
 
 /**
  * Created by Michael on 10.09.2017.
@@ -10,9 +17,31 @@ import android.support.v7.app.AppCompatActivity;
 
 public class StatementofcostsActivity extends AppCompatActivity {
 
+    private Context context;
+    private List<Settle> settelsList;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statementofcosts);
+
+        context = getApplicationContext();
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        this.loadSettles();
+
+        super.onResume();
+    }
+
+    private void loadSettles() {
+        RequestHelper.getAll(context, "/settles", (jsonArray -> {
+            Log.i("settles", jsonArray.toString());
+
+            settelsList = Settle.fromArray(jsonArray);
+        }));
     }
 }
--- app/src/main/java/ch/teko/michael/wgapp/model/Settle.java
+++ app/src/main/java/ch/teko/michael/wgapp/model/Settle.java
@@ -1,6 +1,12 @@
 package ch.teko.michael.wgapp.model;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by Michael on 17.09.2017.
@@ -8,19 +14,49 @@ import java.util.Date;
 
 public class Settle {
 
-    public int id;
-    public String userOwns;
-    public String userLent;
-    public Date date;
-    public boolean payed;
-    public double price;
+    public Integer id;
+    public User userOwns;
+    public User userLent;
+    public String date;
+    public Boolean payed;
+    public Double amount;
 
-    public Settle(int id, String userOwns, String userLent, Date date, boolean payed, double price) {
+    public Settle(Integer id, User userOwns, User userLent, String date, Boolean payed, Double amount) {
         this.id = id;
         this.userOwns = userOwns;
         this.userLent = userLent;
         this.date = date;
         this.payed = payed;
-        this.price = price;
+        this.amount = amount;
+    }
+
+    public static List<Settle> fromArray(JSONArray jsonArray) {
+        List<Settle> settleList = new ArrayList<>();
+
+        try {
+            for (int i = 0; i < jsonArray.length(); i++) {
+                settleList.add(Settle.fromObject(jsonArray.getJSONObject(i)));
+            }
+        } catch (JSONException e) {
+            e.getStackTrace();
+        }
+
+        return settleList;
+    }
+
+    public static Settle fromObject(JSONObject jsonObject) {
+        try {
+            return new Settle(
+                    jsonObject.getInt("id"),
+                    User.fromObject(jsonObject.getJSONObject("userOwns")),
+                    User.fromObject(jsonObject.getJSONObject("userLent")),
+                    jsonObject.getString("date"),
+                    jsonObject.getInt("payed") == 0 ? false : true,
+                    jsonObject.isNull("amount") ? null : jsonObject.getDouble("amount")
+            );
+        } catch (JSONException e) {
+            e.getStackTrace();
+        }
+        return null;
     }
 }
